83b254993807a68ee067e05ada0f5d45da1b7007
Rafael Toledo <rafaeltol@gmail.com>
2022-11-29 08:31:49 -0300

    Add  pass in interface classes

--- aes2/interfaces/interfaces.py
+++ aes2/interfaces/interfaces.py
@@ -11,15 +11,13 @@ Polygon = List[Tuple[int, int]]
 class Camera:
     @abstractmethod
     def get_frame(self) -> Frame:
-        frame = np.zeros_like(Frame)
-        return frame
+        pass
 
 
 class Segmentor:
     @abstractmethod
     def __call__(self, frame) -> Mask:
-        seg_mask = np.zeros_like(Mask)
-        return seg_mask
+        pass
 
 
 class OccupancyGrid:
@@ -31,5 +29,4 @@ class OccupancyGrid:
 class PolygonPath:
     @abstractmethod
     def __call__(self, og_frame: Mask) -> Polygon:
-        polygon_points = [(0, 0), (1, 2), (2, 3)]
-        return polygon_points
+        pass
